e84f85f8dccc4560091aad19fbad2985bf03a61d
EC2 Default User <ec2-user@ip-172-31-29-44.ap-northeast-1.compute.internal>
2023-04-16 12:28:38 +0000

    dentaku

--- main.js
+++ main.js
@@ -6,11 +6,27 @@ function clickbutton(target) {
     result.innerHTML = "0"
   } else if (target_value == "=") {
     result.innerHTML = eval(result.innerHTML)
-  } else {
-    if (result.innerHTML == "0") {
-      result.innerHTML = target_value
+  } else if(target_value == "+" && result.innerHTML.slice(-1) == "+"){
+          let answer = result.innerHTML.slice(0,-1);
+          result.innerHTML = answer  + "+";
+    } else if(target_value == "-" && result.innerHTML.slice(-1) == "-"){
+          let answer = result.innerHTML.slice(0,-1);
+          result.innerHTML = answer  + "-";
+    } else if(target_value == "*" && result.innerHTML.slice(-1) == "*"){
+          let answer = result.innerHTML.slice(0,-1);
+          result.innerHTML = answer  + "*";
+    } else if(target_value == "/" && result.innerHTML.slice(-1) == "/"){
+          let answer = result.innerHTML.slice(0,-1);
+          result.innerHTML = answer  + "/";
     } else {
+    if (result.innerHTML == "0" && "00") {
+      result.innerHTML = target_value
+    } 
+    else {
       result.innerHTML += target_value
     }
   }
 }
+
+
+    
